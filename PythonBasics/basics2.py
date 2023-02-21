a476f36bb2d281d9a0bf2660650f4124438fddc7
Josep Abellana <josep.abe.pu@gmail.com>
2023-02-21 23:55:01 +0100

    x

--- PythonBasics/basics2.py
+++ PythonBasics/basics2.py
@@ -5,3 +5,37 @@ capital = {'brasil': 'brasilia', 'uk': 'london', 'germany': 'berlin'}
 print(capital)
 
 capital['brasil']
+
+## works like in javascript
+
+del capital['germany']
+for k in capital:
+    print(k)
+
+
+text = 'The quick brown fox jumps over the lazy dog'
+
+tokens = text.lower().split(' ')
+print(tokens)
+
+counter = dict()
+for w in tokens:
+    if(w in counter):
+        counter[w]+= 1
+    else:
+        counter[w] = 1
+print(counter)
+
+plant_height = 5
+if plant_height < 3:
+    print('keep it')
+elif (3<plant_height<15):
+    print(plant_height)
+else:
+    print('nothing')
+
+
+#functions
+def count_word(text, characters_to_remove):
+    return
+
