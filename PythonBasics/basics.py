60d51074b19936081eca4793f180a0a86f310dac
Josep Abellana <josep.abe.pu@gmail.com>
2023-02-21 23:27:36 +0100

    x

--- PythonBasics/basics.py
+++ PythonBasics/basics.py
@@ -5,7 +5,7 @@ airport = ['heathrow', 'gatwick', 'stansted']
 print(airport)
 print(airport[0]) # indexing starts at 0
 
-print(airport[3]) # force error
+#print(airport[3]) # force error
 
 airport.extend(['oxford']) # use keyword extend to increase a list
 
@@ -18,3 +18,21 @@ airport_backup = airport[:]
 wrong_airport = airport.pop()
 
 print(airport,airport_backup)
+
+
+for a in airport:
+    print(a.upper())
+
+
+
+# numerical list
+nums = list(range(1,6))
+print(nums)
+
+for x in list(range(5)):
+    print(x**2)
+
+import math # sqrt, log, exp, etc
+
+for x in list(range(5)):
+    print(math.sqrt(x))
